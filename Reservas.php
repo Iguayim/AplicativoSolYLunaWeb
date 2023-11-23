c79378abbc4fe569669fc1f0cba523c56b6ec760
Renzo Calderon <rzocx22@gmail.com>
2023-11-23 16:05:22 -0500

    Reservas.php creada

---
+++ Reservas.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:ital,wght@1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/887a835504.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/Reservas.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>SolYLUNA</title>
+</head>
+ <!-- Encabezado de Reservas de la pagina web del hotel-->
+<body>
+    <header>
+        <div>
+            <div class="info">
+                <ul class="info--lista">
+                    <li class="info--lista__enlace">
+                        <span><i class="fa-solid fa-location-dot"></i></span>
+                        <span>Av.Las lomas Caminos del Inca</span>
+                    </li>
+                    <li class="info--lista__enlace">
+                        <span><i class="fa-solid fa-envelope"></i></span>
+                        <span>Hotelsolyluna@gmail.com</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <nav>
+            <div class="containernav">
+                <div class="container-logo">
+                    <h2>Sol<span>&luna</span></h2>
+                </div>
+
+                <ul class="links">
+                    <li class="link"><a href="Index.php">Inicio</a></li>
+                    <li class="link"><a href="Nosotros.php">Nosotros</a></li>
+                    <li class="link"><a href="servicios.php">Servicios</a></li>
+                    <li class="link"><a href="Habitaciones.php">Habitaciones</a></li>
+                    <li class="link"><a href="Contacto.php">Contacto</a></li>
+                    <li class="link navegacion--lista__activo"><a href="Reservas.php">Reservas</a></li>
+                </ul>
+                <div class="toggle">
+                    |||
+                </div>
+            </div>
+        </nav>
+        <script src="main.js"></script>
+        </div>
+
+    </header>
+    <div class="social">
+        <ul>
+            <li><a href="https://www.facebook.com/profile.php?id=100092658920413" target="_blank" class="icon_facebook"><i class="fa-brands fa-square-facebook fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="https://www.instagram.com/clean_n_dry_pe/" target="_blank" class="icon_instagram"><i class="fa-brands fa-instagram fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="https://api.whatsapp.com/send/?phone=51988924195&text&type=phone_number&app_absent=0" target="_blank" class="icon_whatsapp"><i class="fa-brands fa-whatsapp fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="mailto:contacto@Clean&dry.com" class="icon_google-plus"><i class="fa-brands fa-google-plus-g fa-sm" style="color: #ffffff;"></i></a></li>
+        </ul>
+    </div>
+    <main>
+        <section class="GaranIndex">
+            <div class="contenedor dos-columnas">
+                <div class="Garantiatxt">
+                    <h2>ATELIER</h2>
+                    <h3>Premium suite</h3>
+                    <ul class="deta">
+                        22 - 36 metros cuadrados
+                        (236 - 387 pies cuadrados)
+                    </ul>
+
+                    <p>
+                        Las habitaciones Atelier tienen una terraza privada o
+                        un vestíbulo, además de una amplia zona
+                        de estar. Tres de estas suites están
+                        ubicadas en la parte más nueva de nuestro
+                        hotel, una expansión hacia la galería de
+                        arte contigua. Piense en cuartos de baño
+                        de formato abierto y transitables con
+                        lavabos dobles, ducha de efecto lluvia y
+                        WC separados. Algunas tienen bañeras
+                        independientes con agua caliente.</p>
+
+                    <p>Precio: 200</p>
+
+                    <div class="conoceser">
+                        <button class="agregar-al-carrito" data-nombre="Atelier Premium Suite" data-precio="200" data-imagen="img/HABITACION1.jpg">Agregar al Carrito</button>
+                    </div>
+
+                </div>
+                <div class="imgindex">
+                    <img src="img/HABITACION1.jpg" alt="imagen de garantia">
+                </div>
+            </div>
+        </section>
+
+        <section class="GaranIndex">
+            <div class="contenedor dos-columnas">
+                <div class="Garantiatxt">
+                    <h2>APOSENTO</h2>
+                    <h3>Junior Suite</h3>
+                    <ul class="deta">
+                        22 - 36 metros cuadrados
+                        (236 - 387 pies cuadrados)
+                    </ul>
+
+                    <p>Situado tanto en la mansión histórica como en
+                        el ala contemporánea, con cuartos de baño con
+                        ducha de efecto lluvia, WC separados y jacuzzi(algunos
+                        incluyen una bañera independiente). También hay
+                        una zona de asientos para dos.</p>
+                    <p>Precio: 150</p>
+
+                    <div class="conoceser">
+                        <button class="agregar-al-carrito" data-nombre="Aposento Junior Suite" data-precio="150" data-imagen="images/habitacion2.jpg">Agregar al Carrito</button>
+                    </div>
+                </div>
+                <div class="imgindex">
+                    <img src="images/habitacion2.jpg" alt="imagen de garantia">
+                </div>
+            </div>
+        </section>
+
+        <section class="GaranIndex">
+            <div class="contenedor dos-columnas">
+                <div class="Garantiatxt">
+                    <h2>ALCOBA</h2>
+                    <h3>Habitación estándar</h3>
+                    <ul class="deta">
+                        19 - 22 metros cuadrados
+                        (205 - 236 pies cuadrados)
+
+                    </ul>
+
+                    <p>Ubicadas tanto en la mansión histórica
+                        como en el ala contemporánea,
+                        nuestras suites Alcoba tienen camas
+                        Queen y baños con un solo lavabo,
+                        ducha de efecto lluvia y WC separados.
+                        Hay una zona de estar con espacio para
+                        uno, además de un escritorio.</p>
+                    <p>Precio: 100</p>
+
+                    <div class="conoceser">
+                        <button class="agregar-al-carrito" data-nombre="Alcoba Habitación Estándar" data-precio="100" data-imagen="images/habitacion3.jpg">Agregar al Carrito</button>
+                    </div>
+                </div>
+                <div class="imgindex">
+                    <img src="images/habitacion3.jpg" alt="imagen de garantia">
+                </div>
+            </div>
+        </section>
+        <br>
+        <aside class="carrito">
+            <h2>Carrito de Reservas</h2>
+            <ul class="carrito-lista">
+                <!-- Aquí se agregarán los elementos del carrito -->
+            </ul>
+            <div class="carrito-total">
+                Total: <span id="total-precio">0</span> USD
+            </div>
+            <button class="carrito-boton" id="vaciar-carrito">Vaciar Carrito</button>
+        </aside>
+
+        <div class="form-container">
+
+            <div class="container">
+
+                <form id="hotelReservationForm">
+                    <h1>Reserva de Hotel</h1>
+                    <label for="dni">Número de DNI:</label>
+                    <input type="text" id="dni" name="dni" required>
+
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" required>
+
+                    <label for="apellido">Apellido:</label>
+                    <input type="text" id="apellido" name="apellido" required>
+
+                    <label for="telefono">Teléfono:</label>
+                    <input type="tel" id="telefono" name="telefono" required>
+
+                    <label for="fechaEntrada">Fecha de Entrada:</label>
+                    <input type="date" id="fechaEntrada" name="fechaEntrada" value="2023-11-06" required>
+
+                    <label for="fechaSalida">Fecha de Salida:</label>
+                    <input type="date" id="fechaSalida" name="fechaSalida" value="2023-11-08" required>
+                    <div class="conoceser">
+                        <a href="PAGO.html"><button>Reservar</button></a>
+                    </div>
+            </div>
+            <script src="Reservas.js"></script>
+            </form>
+
+
+
+            <!-- Agrega este formulario en tu página -->
+            <div class="container">
+                <form id="payment-form">
+                    <h1>Información de Pago</h1>
+                    <div id="payment-logos">
+                        <img src="images/pago.jpg" alt="Tarjeta de Crédito" class="tarjeta-logo">
+                    </div>
+
+                    <label for="card-number">Número de Tarjeta:</label>
+                    <input type="text" id="card-number" name="card-number" placeholder="Número de Tarjeta" required>
+
+                    <label for="card-holder">Nombre del Titular:</label>
+                    <input type="text" id="card-holder" name="card-holder" placeholder="Nombre del Titular" required>
+
+                    <label for="expiration-date">Fecha de Vencimiento:</label>
+                    <input type="text" id="expiration-date" name="expiration-date" placeholder="MM/YY" required>
+
+                    <label for="cvv">CVV:</label>
+                    <input type="text" id="cvv" name="cvv" placeholder="CVV" required>
+                    <label for="monto">Monto Total:</label>
+                    <span id="monto-total">0</span> USD
+                    <script src="payment.js"></script>
+
+                    <button>Realizar Pago</button>
+                </form>
+
+            </div>
+    </main>
+    <br>
+
+    <footer class="final">
+        <div class="contenedor box">
+            <div class="columna">
+                <figure class="columna--imagen no-margin">
+                    <a href="index.html"><img src="img/logo.jpg" alt="logo"></a>
+                </figure>
+            </div>
+            <div class="columna">
+                <ul class="columna--lista no-padding">
+                    <li class="columna--lista__flex">
+                        <span><i class="fa-solid fa-location-dot"></i></span>
+                        <span>
+                            Te esperamos en:
+                            <br>
+                            Av.Las lomas Caminos del Inca
+                        </span>
+                    </li>
+                    <li class="columna--lista__flex">
+                        <span><i class="fa-regular fa-calendar-days"></i></span>
+                        <span>
+                            Estamos para ti de:
+                            <br>
+                            Lunes a Sabado: 9:00am - 6:00pm
+                            <br>
+                            Domingo y Feriados: 10:00am - 5:00pm
+                        </span>
+                    </li>
+                </ul>
+            </div>
+            <div class="columna">
+                <h2 class="no-margin underline">Enlaces</h2>
+                <nav>
+                    <ul class="columna--lista no-padding">
+                        <li><a href="Index.php" class="columna--lista__enlace">Inicio</a></li>
+                        <li><a href="Nosotros.php" class="columna--lista__enlace">Nosotros</a></li>
+                        <li><a href="servicios.php" class="columna--lista__enlace">Servicios</a></li>
+                        <li><a href="Habitaciones.php" class="columna--lista__enlace">Habitaciones</a></li>
+                        <li><a href="Contacto.php" class="columna--lista__enlace">Contacto</a></li>
+                        <li><a href="Reservas.php" class="columna--lista__enlace">Reservas</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="columna">
+                <h2 class="no-margin underline">Contacto</h2>
+                <p>Atención al Cliente:
+                    <br>
+                    Hotelsolyluna@gmail.com
+                </p>
+            </div>
+        </div>
+
+        <div class="copy">
+            <p class="copyright">&copy Copyright Sol & Luna - 2023</p>
+        </div>
+    </footer>
+</body>
+
+</html>
