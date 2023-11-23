65030e1a490652d3f99850e3411d5d2013ef533b
Renzo Calderon <rzocx22@gmail.com>
2023-11-23 16:08:43 -0500

    servicios.php creada

---
+++ servicios.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:ital,wght@1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/887a835504.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/servicio.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>SolYLUNA</title>
+</head>
+ <!-- Encabezado de Sericios de la pagina web-->
+<body>
+    <header>
+        <div>
+            <div class="info">
+                <ul class="info--lista">
+                    <li class="info--lista__enlace">
+                        <span><i class="fa-solid fa-location-dot"></i></span>
+                        <span>Av.Las lomas Caminos del Inca</span>
+                    </li>
+                    <li class="info--lista__enlace">
+                        <span><i class="fa-solid fa-envelope"></i></span>
+                        <span>Hotelsolyluna@gmail.com</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <nav>
+            <div class="containernav">
+
+                <div class="container-logo">
+                    <h2>SOL<span>&LUNA</span></h2>
+                </div>
+
+                <ul class="links">
+                    <li class="link"><a href="Index.php">Inicio</a></li>
+                    <li class="link"><a href="Nosotros.php">Nosotros</a></li>
+                    <li class="link navegacion--lista__activo"><a href="servicios.php">Servicios</a></li>
+                    <li class="link"><a href="Habitaciones.php">Habitaciones</a></li>
+                    <li class="link"><a href="Contacto.php">Contacto</a></li>
+                    <li class="link"><a href="Reservas.php">Reservas</a></li>
+                </ul>
+                <div class="toggle">
+                    |||
+                </div>
+            </div>
+        </nav>
+        <script src="main.js"></script>
+        </div>
+
+    </header>
+
+    <div class="social">
+        <ul>
+            <li><a href="https://www.facebook.com/profile.php?id=100092658920413" target="_blank" class="icon_facebook"><i class="fa-brands fa-square-facebook fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="https://www.instagram.com/clean_n_dry_pe/" target="_blank" class="icon_instagram"><i class="fa-brands fa-instagram fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="https://api.whatsapp.com/send/?phone=51988924195&text&type=phone_number&app_absent=0" target="_blank" class="icon_whatsapp"><i class="fa-brands fa-whatsapp fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="mailto:contacto@Clean&dry.com" class="icon_google-plus"><i class="fa-brands fa-google-plus-g fa-sm" style="color: #ffffff;"></i></a></li>
+        </ul>
+    </div>
+
+    <main class="contenedor">
+        <h3 class="centrar-texto">Servicios</h3>
+        <div class="sobre-servicios">
+            <div class="sobre-servicios__imagen" id="1">
+                <img src="img/servicio1.jpg" alt="imagen servicio">
+            </div>
+            <div class="sobre-servicios__texto">
+                <h4 class="no-margin">JACUZI</h4>
+                <p style="text-align: justify;">
+                    El servicio de jacuzzi es una experiencia de
+                    relajación y bienestar que ofrece a los huespedes
+                    la oportunidad de sumergirse en una bañera especialmente
+                    diseñada con agua caliente y burbujas. Estos jacuzzis
+                    suelen estar equipados con jets de agua y aire que
+                    proporcionan un masaje suave y terapéutico en el cuerpo,
+                    aliviando la tensión muscular y promoviendo la
+                    relajación.<br>
+                    Los jacuzzis pueden variar en tamaño, desde jacuzzis
+                    privados para dos personas en habitaciones de hotel
+                    hasta jacuzzis más grandes en áreas de spa o piscinas
+                    de hoteles. Los beneficios de utilizar un jacuzzi
+                    incluyen la mejora de la circulación sanguínea, la
+                    reducción del estrés, el alivio de dolores musculares
+                    y articulares, y la sensación general de relajación y
+                    bienestar.
+            </div><!--un servicio-->
+        </div>
+
+        <div class="sobre-servicios">
+            <div class="sobre-servicios__texto">
+                <h4 class="no-margin">HABITACIONES CON JACUZZI PRIVADO</h4>
+                <p style="text-align: justify;">
+                    ofrecen habitaciones especiales que incluyen un jacuzzi
+                    privado en la habitación. Estas habitaciones suelen
+                    ser populares entre parejas que buscan una experiencia
+                    romántica y relajante..
+                </p>
+            </div><!--un servicio-->
+
+            <div class="sobre-servicios__texto">
+                <h4 class="no-margin">ZONAS COMUNES</h4>
+                <p style="text-align: justify;">
+                    jacuzzis en áreas comunes para que los huéspedes
+                    los utilicen de forma gratuita. Estos jacuzzis suelen
+                    estar cerca de la piscina o en una terraza con hermosas
+                    vistas.
+                </p>
+            </div><!--un servicio-->
+        </div>
+
+        <div class="sobre-servicios">
+            <div class="sobre-servicios__imagen">
+                <img src="img/servicio2.jpg" alt="imagen servicio">
+            </div>
+            <div class="sobre-servicios__texto">
+                <h4 class="no-margin">AGUA CALIENTE</h4>
+                <p style="text-align: justify;">
+                    En nuestro hotel, el agua caliente de
+                    nuestros jacuzzis es el camino hacia
+                    la paz interior y el rejuvenecimiento.
+                    Te esperamos para brindarte una experiencia
+                    que recordarás para siempre.
+                </p>
+                <p style="text-align: justify;">
+                    En nuestro hotel, cada habitación está equipada con
+                    agua caliente al instante, lo que te permite
+                    disfrutar de relajantes duchas y baños cuando lo
+                    desees. Sumérgete en la comodidad y la relajación
+                    después de un largo día. Algunas habitaciones
+                    también cuentan con jacuzzis privados para una
+                    experiencia de lujo adicional. Tu bienestar es
+                    nuestra prioridad. ¡Reserva ahora y sumérgete en
+                    la calidez y el confort del agua caliente en tu
+                    propia habitación!
+                </p>
+            </div><!--un servicio-->
+        </div>
+
+        <div class="sobre-servicios">
+            <div class="sobre-servicios__imagen">
+                <img src="img/servicio3.jpg" alt="imagen servicio">
+            </div>
+            <div class="sobre-servicios__texto">
+                <h4 class="no-margin">TV</h4>
+                <p style="text-align: justify;">
+                    En nuestro hotel, te ofrecemos una experiencia de
+                    hospedaje completa y cómoda. Cada habitación está
+                    equipada con un televisor de alta definición para que
+                    disfrutes de tus programas y películas favoritas
+                    después de un día lleno de actividades. Relájate y
+                    sumérgete en la comodidad mientras te ponemos a tu
+                    disposición un mundo de entretenimiento a tu alcance.
+                </p>
+                <P style="text-align: justify;">
+                    Además de la calidad visual, te brindamos una
+                    variedad de canales de cable y satélite para
+                    que encuentres exactamente lo que deseas ver.
+                    Ya sea que estés interesado en las últimas
+                    noticias, películas recién estrenadas o eventos
+                    deportivos en vivo, tenemos opciones para todos
+                    los gustos. En definitiva, tu estancia en nuestro
+                    hotel se enriquece con la inclusión de un televisor
+                    de alta definición en tu habitación, ofreciéndote una
+                    experiencia de entretenimiento y comodidad que supera
+                    tus expectativas. ¡Reserva tu estancia hoy mismo y
+                    experimenta el placer de disfrutar de las comodidades
+                    de alta gama!
+                </P>
+            </div><!--un servicio-->
+        </div>
+
+    </main>
+
+    <footer class="final">
+        <div class="contenedor box">
+            <div class="columna">
+                <figure class="columna--imagen no-margin">
+                    <a href="Index.php"><img src="img/logo.jpg" alt="logo"></a>
+                </figure>
+            </div>
+            <div class="columna">
+                <ul class="columna--lista no-padding">
+                    <li class="columna--lista__flex">
+                        <span><i class="fa-solid fa-location-dot"></i></span>
+                        <span>
+                            Te esperamos en:
+                            <br>
+                            Av.Las lomas Caminos del Inca
+                        </span>
+                    </li>
+                    <li class="columna--lista__flex">
+                        <span><i class="fa-regular fa-calendar-days"></i></span>
+                        <span>
+                            Estamos para ti de:
+                            <br>
+                            Lunes a Sabado: 9:00am - 6:00pm
+                            <br>
+                            Domingo y Feriados: 10:00am - 5:00pm
+                        </span>
+                    </li>
+                </ul>
+            </div>
+            <div class="columna">
+                <h2 class="no-margin underline">Enlaces</h2>
+                <nav>
+                    <ul class="columna--lista no-padding">
+                        <li><a href="Index.php" class="columna--lista__enlace">Inicio</a></li>
+                        <li><a href="Nosotros.php" class="columna--lista__enlace">Nosotros</a></li>
+                        <li><a href="servicios.php" class="columna--lista__enlace">Servicios</a></li>
+                        <li><a href="Habitaciones.php" class="columna--lista__enlace">Habitaciones</a></li>
+                        <li><a href="Contacto.php" class="columna--lista__enlace">Contacto</a></li>
+                        <li><a href="Reservas.php" class="columna--lista__enlace">Reservas</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="columna">
+                <h2 class="no-margin underline">Contacto</h2>
+                <p>Atención al Cliente:
+                    <br>
+                    Hotelsolyluna@gmail.com
+                </p>
+            </div>
+        </div>
+
+        <div class="copy">
+            <p class="copyright">&copy Copyright SolYLUNA - 2023</p>
+        </div>
+    </footer>
+</body>
+
+</html>
