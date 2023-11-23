c2a0240e3958484150996b508b25bba209a58b92
Nayely Ñacayauri <nayelynacayauridelacruz@gmail.com>
2023-11-23 16:40:40 -0500

    Insercion del chatbox

--- Index.php
+++ Index.php
@@ -13,7 +13,8 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="https://kit.fontawesome.com/887a835504.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script>
+   <!-- Inserccion del chatbox en la pagina web-->
+   <script>
         window.addEventListener('mouseover', initLandbot, {
             once: true
         });
