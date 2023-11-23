78e21e001b0fd926b7796d46d448901618087fde
Renzo Calderon <rzocx22@gmail.com>
2023-11-23 16:04:27 -0500

    Nosotros.php creada

---
+++ Nosotros.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:ital,wght@1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/887a835504.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/nosotros.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>SolYLUNA</title>
+</head>
+ <!-- Encabezado de Nosotros de la pagina web -->
+<body>
+    <header>
+        <div>
+            <div class="info">
+                <ul class="info--lista">
+                    <li class="info--lista__enlace">
+                        <span><i class="fa-solid fa-location-dot"></i></span>
+                        <span>Av.Las lomas Caminos del Inca</span>
+                    </li>
+                    <li class="info--lista__enlace">
+                        <span><i class="fa-solid fa-envelope"></i></span>
+                        <span>Hotelsolyluna@gmail.com</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <nav>
+            <div class="containernav">
+                <div class="container-logo">
+                    <h2>Sol<span>&luna</span></h2>
+                </div>
+
+                <ul class="links">
+                    <li class="link"><a href="Index.php">Inicio</a></li>
+                    <li class="link navegacion--lista__activo"><a href="Nosotros.php">Nosotros</a></li>
+                    <li class="link"><a href="servicios.php">Servicios</a></li>
+                    <li class="link"><a href="Habitaciones.php">Habitaciones</a></li>
+                    <li class="link"><a href="Contacto.php">Contacto</a></li>
+                    <li class="link"><a href="Reservas.php">Reservas</a></li>
+                </ul>
+                <div class="toggle">
+                    |||
+                </div>
+            </div>
+        </nav>
+        <script src="main.js"></script>
+        </div>
+
+    </header>
+    <div class="social">
+        <ul>
+            <li><a href="https://www.facebook.com/profile.php?id=100092658920413" target="_blank" class="icon_facebook"><i class="fa-brands fa-square-facebook fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="https://www.instagram.com/clean_n_dry_pe/" target="_blank" class="icon_instagram"><i class="fa-brands fa-instagram fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="https://api.whatsapp.com/send/?phone=51988924195&text&type=phone_number&app_absent=0" target="_blank" class="icon_whatsapp"><i class="fa-brands fa-whatsapp fa-lg" style="color: #ffffff;"></i></a></li>
+            <li><a href="mailto:contacto@Clean&dry.com" class="icon_google-plus"><i class="fa-brands fa-google-plus-g fa-sm" style="color: #ffffff;"></i></a></li>
+        </ul>
+    </div>
+
+    <main>
+        <section class="connosotros-fondo">
+            <div class="connosotros">
+                <h2>SOBRE NOSOTROS</h2>
+                <div class="cardcontenedor"></div>
+                <div class="card parpadeo">
+                    <div class="face front">
+                        <img src="img/img1.jpg" alt="">
+                        <h3>MISION</h3>
+                    </div>
+                    <div class="face back">
+                        <h3>MISION</h3>
+                        <P>Nuestra misión es ofrecer a nuestros huéspedes una estancia inolvidable, brindándoles un servicio de alta calidad, comodidad y atención personalizada. Nos esforzamos por superar las expectativas de nuestros clientes en cada visita, cuidando de cada detalle para crear recuerdos felices y duraderos.</P>
+                        <div class="link">
+                            <a href="#">Datails</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card parpadeo">
+                    <div class="face front">
+                        <img src="img/img2.jpg" alt="">
+                        <h3>VISION</h3>
+                    </div>
+                    <div class="face back">
+                        <h3>VISION</h3>
+                        <P>La visión de nuestro hotel es convertirse en el destino preferido de viajeros y huéspedes locales que buscan una experiencia de hospitalidad excepcional en un entorno único y relajante. Queremos ser reconocidos por nuestro compromiso con la satisfacción del cliente, la excelencia en el servicio.</P>
+                        <div class="link">
+                            <a href="#">Datails</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card parpadeo">
+                    <div class="face front">
+                        <img src="img/img3.jpg" alt="">
+                        <h3>OBJETIVOS</h3>
+                    </div>
+                    <div class="face back">
+                        <h3>OBJETIVOS</h3>
+
+                        <li>satisfacción del cliente:</li>
+                        <p>
+                            Garantizar que los huéspedes tengan una experiencia positiva.
+                        </p>
+                        <li>Rentabilidad:</li>
+                        <p>
+                            Mantener y mejorar la rentabilidad es esencial para la supervivencia del hotel.
+                        </p>
+
+                        <li>Fidelización de clientes:</li>
+                        <p>
+                            Atraer a clientes recurrentes y crear sclientes leales.
+                        </p>
+
+                        </p>
+                        <div class="link">
+                            <a href="#">Datails</a>
+                        </div>
+                    </div>
+                </div>
+
+    </main>
+
+    <footer class="final">
+        <div class="contenedor box">
+            <div class="columna">
+                <figure class="columna--imagen no-margin">
+                    <a href="index.html"><img src="img/logo.jpg" alt="logo"></a>
+                </figure>
+            </div>
+            <div class="columna">
+                <ul class="columna--lista no-padding">
+                    <li class="columna--lista__flex">
+                        <span><i class="fa-solid fa-location-dot"></i></span>
+                        <span>
+                            Te esperamos en:
+                            <br>
+                            Av.Las lomas Caminos del Inca
+                        </span>
+                    </li>
+                    <li class="columna--lista__flex">
+                        <span><i class="fa-regular fa-calendar-days"></i></span>
+                        <span>
+                            Estamos para ti de:
+                            <br>
+                            Lunes a Sabado: 9:00am - 6:00pm
+                            <br>
+                            Domingo y Feriados: 10:00am - 5:00pm
+                        </span>
+                    </li>
+                </ul>
+            </div>
+            <div class="columna">
+                <h2 class="no-margin underline">Enlaces</h2>
+                <nav>
+                    <ul class="columna--lista no-padding">
+                        <li><a href="Index.php" class="columna--lista__enlace">Inicio</a></li>
+                        <li><a href="Nosotros.php" class="columna--lista__enlace">Nosotros</a></li>
+                        <li><a href="servicios.php" class="columna--lista__enlace">Servicios</a></li>
+                        <li><a href="Habitaciones.php" class="columna--lista__enlace">Habitaciones</a></li>
+                        <li><a href="Contacto.php" class="columna--lista__enlace">Contacto</a></li>
+                        <li><a href="Reservas.php" class="columna--lista__enlace">Reservas</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="columna">
+                <h2 class="no-margin underline">Contacto</h2>
+                <p>Atención al Cliente:
+                    <br>
+                    Hotelsolyluna@gmail.com
+                </p>
+            </div>
+        </div>
+
+        <div class="copy">
+            <p class="copyright">&copy Copyright Sol & Luna - 2023</p>
+        </div>
+    </footer>
+</body>
+
+</html>
